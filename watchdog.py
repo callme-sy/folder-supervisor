da6873c0d955120bf70034161e0d5799d1fdfdbd
Augustin <62944382+antonio20d@users.noreply.github.com>
2020-11-20 08:54:30 +0100

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -7,8 +7,9 @@ from watchdog.utils import stat as default_stat
 
 [docs]class DirectorySnapshotDiff(object):
     """
-    Compares two directory snapshots and creates an object that represents
-    the difference between the two snapshots.
+
+    Compare deux snapshots de répertoire et crée un objet qui représente
+    la différence entre les deux snapshots. 
 
     :param ref:
         The reference directory snapshot.
@@ -25,13 +26,13 @@ from watchdog.utils import stat as default_stat
         created = snapshot.paths - ref.paths
         deleted = ref.paths - snapshot.paths
         
-        # check that all unchanged paths have the same inode
+        # vérifier que tous les chemins inchangés ont le même inode
         for path in ref.paths & snapshot.paths:
             if ref.inode(path) != snapshot.inode(path):
                 created.add(path)
                 deleted.add(path)
         
-        # find moved paths
+        # trouver des chemins déplacés
         moved = set()
         for path in set(deleted):
             inode = ref.inode(path)
